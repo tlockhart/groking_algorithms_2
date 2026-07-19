0a2da20cf56c48a2b2731036b3b83992dee33a38
Tony Lockhart <tony.lockhart@ymail.com>
2026-07-19 14:49:29 -0400

    Update max_num_20241224.py

--- chapter3/algorithms/recursion/maxNum/max_num_20241224.py
+++ chapter3/algorithms/recursion/maxNum/max_num_20241224.py
@@ -1,13 +1,22 @@
+# What is the largets number in the List of numbers?
+
 def max_num(numbers):
-    # base case
-    if number == 0:
+    # Base cases
+    if len(numbers) == 1:
+        return numbers[0]
+
+    if len(numbers) == 0:
         return 0
-        
-    # init
-    max = numbers[0]
-    
-    # recursive case
-    # Recurse though the array fo find the largest number
-    
-    equation = max_num((n-1) + (n-2))
-    return equation
+
+    # Recursive case
+    max_num = numbers.pop(0)      # Remove the first element
+    next_num = max_num(numbers)
+
+    if max_num > next_num:
+        return max_num
+    else:
+        return next_num
+
+
+numbers = [1, 2, 3, 4]
+print("max_num:", max_num(numbers))
